e6d9b94d3f09497f38fbd8e42c0a4f70de7f18a5
HassanS6000 <hassansyyid@gmail.com>
2015-10-08 16:58:53 -0400

    Updated to v1.8 - removed a random Random.

--- src/main/java/com/negafinity/ironhawk/entities/Bomber.java
+++ src/main/java/com/negafinity/ironhawk/entities/Bomber.java
@@ -1,7 +1,5 @@
 package com.negafinity.ironhawk.entities;
 
-import java.util.Random;
-
 import com.negafinity.ironhawk.Controller;
 import com.negafinity.ironhawk.Game;
 import com.negafinity.ironhawk.Textures;
@@ -13,8 +11,7 @@ public class Bomber extends Enemy
 	{
 		super(x, y, tex, c, game, enemyHealth);
 
-		Random r = new Random();
-		this.name = "B 29 Bomber";
+		this.name = "B-29 Bomber";
 		this.enemyHealth = 500;
 		anim = new Animation(5, tex.bomber[0], tex.bomber[1]);
 
@@ -24,10 +21,12 @@ public class Bomber extends Enemy
 	public void tick()
 	{
 		super.tick();
-		while (this.x > 0 &&this.x < 640)
+		
+		while (this.x > 0 && this.x < 640)
 		{
 			x++;
 		}
+		
 		if(this.x == 640)
 		{
 			while (this.x > 0 && this.x < 640)
